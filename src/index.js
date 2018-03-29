4baec27ae7a687e4f2239b499e90270f7ba4931a
FallenLight <xFallenLight@users.noreply.github.com>
2018-03-29 11:48:08 +0800

    Code + string indentifier

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-
+class Type {
+  constructor(input) {
+    this.input = input
+  }
+  
+  _type_string(input) {
+    if(typeof input === 'string') {
+      return "String";
+    } else {
+      return "Nope";
+  }
+}
