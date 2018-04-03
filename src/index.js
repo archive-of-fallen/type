d0db858e579790b3b11aa73af89be09bb517c1dc
FallenLight <xFallenLight@users.noreply.github.com>
2018-04-03 16:30:23 +0800

    Nest switch/case within a function

--- src/index.js
+++ src/index.js
@@ -3,17 +3,19 @@ class Type {
     this.input = input
   }
   
-  switch(input) {
-    case 'string':
-      console.log('String');
-      break;
-    case 'boolean':
-      console.log('Boolean');
-      break;
-    case 'number':
-      console.log('Integer');
-      break;
-    default:
-      console.log('Sorry, I don\'t recognize your input');
+  type(input) {
+    switch(input) {
+      case 'string':
+        console.log('String');
+        break;
+      case 'boolean':
+        console.log('Boolean');
+        break;
+      case 'number':
+        console.log('Integer');
+        break;
+      default:
+        console.log('Sorry, I don\'t recognize your input');
+    }
   }
 }
