311a0be78e8e20011996b90f2ef042c222cbeee0
FallenLight <xFallenLight@users.noreply.github.com>
2018-04-07 15:55:36 +0800

    Update

--- src/index.js
+++ src/index.js
@@ -1,21 +1,3 @@
-class Type {
-  constructor(input) {
-    this.input = input
-  }
-  
-  type(input) {
-    switch(input) {
-      case 'string':
-        console.log('String');
-        break;
-      case 'boolean':
-        console.log('Boolean');
-        break;
-      case 'number':
-        console.log('Integer');
-        break;
-      default:
-        console.log('Sorry, I don\'t recognize your input');
-    }
-  }
-}
+/****
+Probably going to use this later
+****/
