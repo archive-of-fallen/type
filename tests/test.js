11160892783353740efdf7784cb7762d6af27625
Fallen <30222397+whomhasfallen@users.noreply.github.com>
2018-07-08 15:45:58 +0800

    Use Lunar's suggestion

--- tests/test.js
+++ tests/test.js
@@ -1,4 +1,2 @@
-const Type = require('../src/index.js');
-// Need to do the rest
-
-// You can do `const { type } = require('../src/index.js');` too
+const { type } = require('../src/index.js');
+// Need to finish off the rest 
